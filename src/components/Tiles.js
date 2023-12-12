a0b78f41408b84ecf83bea373b5bd022ded1f612
Tony Eder <tonyeder11@gmail.com>
2023-12-12 15:30:19 -0800

    pig tiles dialed

--- src/components/Tiles.js
+++ src/components/Tiles.js
@@ -3,13 +3,11 @@ import React from 'react'
 function Tiles({ name, specialty, greased, image, weight, medal  }) {
     
     return (
-        <div className="ui eight wide column" key={name}>
+        <div className="ui four wide column">
+            <div className="pigTile" key={name}>
             <p>Name: {name}</p>
-            <img alt={name} src={image}></img>
-            <p>Weight: {weight}</p>
-            <p>Medals Won: {medal}</p>
-            <p>Specialties: {specialty}</p>
-            <p>Greased: {greased}</p>
+            <img className="minPigTile" alt={name} src={image}></img>
+            </div>
         </div>
     )
 }
